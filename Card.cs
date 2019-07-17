a404c264d21768e0a5fe50f49aa5c890341b13b4
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-07-17 11:08:11 -0600

    created initial deck

--- Card.cs
+++ Card.cs
@@ -1,7 +1,52 @@
 namespace blackjack
 {
+    using System;
+
+    //Represents the suit of a card
+    public enum CardSuit
+    {
+        Unknown = 0,
+        //Unknown Card suit
+
+        Heart = 1,
+        Club = 2,
+        Spade = 3,
+        Diamond = 4
+    }
+
+    /// Contains information about a playing card. ///
     public class Card
     {
-        
+
+        //Initializes a new instance of the Card class
+        public Card(CardSuit suit, int rank)
+        {
+            this.Suit = suit;
+            this.Rank = rank;
+        }
+        // Gets or Sets values of playing card rank
+        public int Rank
+        {
+            get; private set;
+        }
+
+        //Gets or Sets suit of the card
+        public CardSuit Suit { get; private set; }
+
+
+        //Returns a true value if card suit is red or false if black
+        public bool IsRed
+        {
+            get
+            {
+                return this.Suit == CardSuit.Heart || this.Suit == CardSuit.Diamond;
+            }
+        }
+
+        public void PrintCardToScreen()
+        {
+            Console.WriteLine($"Suit: {this.Suit}, Rank: {this.Rank}");
+        }
     }
 }
+
--- Program.cs
+++ Program.cs
@@ -1,12 +1,26 @@
-﻿using System;
-
+﻿
 namespace blackjack
 {
-    class Program
+using System;
+    using System.Collections.Generic;
+
+    public class Program
     {
-        static void Main(string[] args)
+        //Main blackjack program  -- entry point
+        public static void Main(string[] args)
         {
-            
+            List<Card> deck = new List<Card>();
+            for (int s = 1; s < 5; s++)
+            {
+                CardSuit suit = (CardSuit)s;
+                for (int r = 1; r < 14; r++){
+                    Card card = new Card(suit, r);
+                    deck.Add(card);
+                }
+            }
+            foreach(Card card in deck){
+                card.PrintCardToScreen();
+            }
         }
     }
 }
